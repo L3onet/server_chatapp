02f343489eb6f2082f2b7c7de786e441cc54c0f0
Leonel Gonzalez Vidales <leonelgv@MacBook-Pro-de-Leonel.local>
2025-02-11 10:29:54 -0600

    Proyecto inicial

---
+++ app.js
@@ -0,0 +1,7 @@
+import express from "express";
+import http from "http";
+import { initSocketServer } from "./utils/index.js";
+const app = express();
+const server = http.createServer(app);
+initSocketServer(server);
+export { server };
---
+++ index.js
@@ -0,0 +1,8 @@
+import { server } from "./app.js";
+import { IP_SERVER, PORT } from "./constants.js";
+server.listen(PORT, () => {
+    console.log("######################");
+    console.log("###### API REST ######");
+    console.log("######################");
+    console.log(`http://${IP_SERVER}:${PORT}/api`);
+});
