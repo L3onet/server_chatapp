ceb931a0c53a6fbdf2df4e4db9ec4f1e04f24b7b
Leonel Gonzalez Vidales <leonelgv@live.com>
2025-02-19 11:46:13 -0600

    Feat: Autenticacion implementada

--- constants.js
+++ constants.js
@@ -3,3 +3,5 @@ export const PORT = process.env.PORT || 3977;
 export const DB_USER = "usuario";
 export const DB_PASSWORD = "password";
 export const DB_HOST = "servidor";
+
+export const JWT_SECRET_KEY = "kjlalksjdljads8789a79ahsdk789sahkjas668798";
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 import { User } from "../models/index.js";
 import bscrypt from "bcryptjs";
+import { jwt } from "../utils/jwt.js";
 
 async function register(req, res) {
     try {
@@ -24,7 +25,64 @@ async function register(req, res) {
     }
 }
 
+async function login(req, res) {
+    try {
+        const { email, password } = req.body;
+        const emailLowerCase = email.toLowerCase();
+
+        const userStorage = await User.findOne({ email: emailLowerCase });
+        if (!userStorage) {
+            return res.status(400).send({ msg: "Usuario no encontrado" });
+        }
+
+        const isPasswordValid = await bscrypt.compare(password, userStorage.password);
+        if (!isPasswordValid) {
+            return res.status(400).send({ msg: "Contraseña incorrecta" });
+        }
+
+        // Crear los tokens
+        res.status(200).send({
+            access: jwt.createAccessToken(userStorage),
+            refresh: jwt.createRefreshToken(userStorage),
+        });
+
+    } catch (error) {
+        res.status(500).send({ msg: "Error del servidor", error: error.message });
+    }
+}
+
+async function refreshAccessToken(req, res) {
+    try {
+        const { refreshToken } = req.body;
+
+        if (!refreshToken) {
+            return res.status(400).send({ msg: "Token requerido" });
+        }
+
+        const hasExpired = jwt.hasExpiredToken(refreshToken);
+        if (hasExpired) {
+            return res.status(400).send({ msg: "Token expirado" });
+        }
+
+        const { user_id } = jwt.decoded(refreshToken);
+
+        // Ahora usamos await para encontrar el usuario sin callbacks
+        const userStorage = await User.findById(user_id);
+        if (!userStorage) {
+            return res.status(400).send({ msg: "Usuario no encontrado" });
+        }
+
+        res.status(200).send({
+            accessToken: jwt.createAccessToken(userStorage),
+        });
+    } catch (error) {
+        res.status(500).send({ msg: "Error del servidor", error: error.message });
+    }
+}
+
 
 export const AuthController = {
     register,
+    login, 
+    refreshAccessToken,
 };
