254dfeb256d432df26df76f2fefc666d79c42541
Leonel Gonzalez Vidales <leonelgv@live.com>
2025-05-29 12:25:28 -0600

    Feat: Deploy server

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 export const IP_SERVER = "localhost";
 export const PORT = process.env.PORT || 3977;
-export const DB_USER = "usuario";
-export const DB_PASSWORD = "password";
-export const DB_HOST = "servidor";
+export const DB_USER = "natalie";
+export const DB_PASSWORD = "v4OO5D8JPcJlkMkX";
+export const DB_HOST = "cluster0.t6epd.mongodb.net";
 
 export const JWT_SECRET_KEY = "kjlalksjdljads8789a79ahsdk789sahkjas668798";
